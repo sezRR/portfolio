54f52e6a74b8b6aff0988b86d34b96b9441dc55b
szRR <sezer.tetik2005@gmail.com>
2022-08-10 23:40:37 +0300

    fix build error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({Component, pageProps}: AppProps) {
                 <link rel="icon" href="/static/images/favicon.ico"/>
 
                 <style>
-                    @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap');
+                    @import url(&quot;https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap&quot;);
                 </style>
             </Head>
 
