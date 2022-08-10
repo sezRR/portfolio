445ff49b9ac8ce4eb5b1a2e4633cbe6295310f3d
szRR <sezer.tetik2005@gmail.com>
2022-08-10 23:35:51 +0300

    add loading page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <Head>
+                <title>Sezer Tetik</title>
+                <meta name="description" content="Sezer Tetik's portfolio website"/>
+                <link rel="icon" href="/static/images/favicon.ico"/>
+
+                <style>
+                    @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap');
+                </style>
+            </Head>
+
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
---
+++ pages/loading.tsx
@@ -0,0 +1,19 @@
+import type {NextPage} from 'next'
+import styles from '../styles/LoadingPage.module.css'
+
+const LoadingPage: NextPage = () => {
+    return (
+        <main className={styles.mainContainer}>
+            <div className={styles.informationContainer}>
+                <h6 className={styles.jobInformation}>Software Developer</h6>
+                <header className={styles.information}>Sezer Tetik</header>
+                <div className={styles.loadingMarker}>
+                    <div className={styles.loadingMarkerSettings}></div>
+                </div>
+            </div>
+
+        </main>
+    )
+}
+
+export default LoadingPage
